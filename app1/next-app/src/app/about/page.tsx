d47d4a457ab578378d8dc735fd255c0d74059ec4
Steve Jeong <jst0930@gmail.com>
2025-04-01 15:07:11 +0300

    initial setup

---
+++ app1/next-app/src/app/about/page.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1 className="text-5xl text-blue-600">App1 About</h1>
+      <Image src="/pwa-app1.jpg" alt="Logo" width={512} height={512} />
+      <Link
+        href="/"
+        className="text-xl bg-amber-500 hover:bg-amber-300 px-3 py-2 rounded-lg border-2"
+      >
+        Go Home
+      </Link>
+    </div>
+  );
+};
+
+export default About;
---
+++ app1/next-app/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-5xl text-blue-600">App1 in the root</h1>
+      <Link href="/about" className="text-xl bg-amber-500 hover:bg-amber-300 px-3 py-2 rounded-lg border-2">About</Link>
+    </div>
+  );
+}

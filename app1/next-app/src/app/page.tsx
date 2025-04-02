416d1c924943f8e82d0a2d547745e6bd3d9d60a9
Steve Jeong <jst0930@gmail.com>
2025-04-02 06:04:15 +0300

    app1 privacy page added

--- app1/next-app/src/app/page.tsx
+++ app1/next-app/src/app/page.tsx
@@ -1,10 +1,12 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-5xl text-blue-600">App1 in the root</h1>
-      <Link href="/about" className="text-xl bg-amber-500 hover:bg-amber-300 px-3 py-2 rounded-lg border-2">About</Link>
+    <div className="flex flex-col">
+      <h1 className="text-5xl text-blue-600 p-4">App1 in the root</h1>
+      <Link href="/about" className="text-xl bg-amber-500 hover:bg-amber-300 px-8 py-4 m-4 rounded-lg w-fit border-2">About</Link>
+      <Image src="/app1/icons/web-app-manifest-512x512.png" alt="Logo" width={512} height={512} />
     </div>
   );
 }
---
+++ app1/next-app/src/app/privacy/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Privacy = () => {
+  return (
+    <div className='text-3xl font-extrabold text-blue-600'>Privacy</div>
+  )
+}
+
+export default Privacy
